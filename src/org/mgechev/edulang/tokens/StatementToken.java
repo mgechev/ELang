a52c971206a11252fc1090c519406ea8ffc0f7ac
Minko Gechev <mdgechev@gmail.com>
2012-09-15 23:53:19 +0300

    Few built in functions are added. The algorithm used for creating expressions in RPN is changed (currently the Shunting yard algorithm is used). Actually the evaluation is a little buggy (the comment in the Main class). TODO: refactoring (Symbols == flyweight?), custom functions, includes, fixing the evaluation issue.

--- src/org/mgechev/edulang/tokens/StatementToken.java
+++
@@ -1,9 +0,0 @@
-package org.mgechev.edulang.tokens;
-
-public class StatementToken extends Token<String> {
-    
-    public StatementToken(String symbol) {
-        this.value = symbol;
-    }
-    
-}
