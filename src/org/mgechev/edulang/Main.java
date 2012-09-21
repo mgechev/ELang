edea9d86eb4aecebc3e8b7e4c0eb36c4657c1de3
Minko Gechev <mdgechev@gmail.com>
2012-09-21 22:50:50 +0300

    Returning the right state of the main method...

--- src/org/mgechev/edulang/Main.java
+++ src/org/mgechev/edulang/Main.java
@@ -3,19 +3,7 @@ package org.mgechev.edulang;
 public class Main {
 
     public static void main(String[] args) {
-        //EduLang.loadProgramFile(args[0]);
-        String program =
-                "current = 0;" +
-                "n = 7;" +
-                "while current < n:" +
-                "if current % 2 == 0:" +
-                "print current;" +
-                "endif;" +
-                "print '\n';" +
-                "current = current + 1;" +
-                "endwhile;";
-
-        EduLang.loadProgramString(program);
+        EduLang.loadProgramFile(args[0]);
         EduLang.execute();
     }
 
